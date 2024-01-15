de0d02e3dff19a8383490721b978698309c343b9
Yusuke-masyumaro <mattynn1018@gmail.com>
2024-01-15 15:23:43 +0900

    add_params

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 from diffusers import DDPMScheduler
 import numpy as np
 import torch
-import librosa
-import soundfile as sf
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.multiprocessing as mp
 import torch.distributed as dist
@@ -10,13 +8,13 @@ from torch.utils.data import Dataset, DataLoader
 import torchaudio
 from tqdm import tqdm
 import pandas as pd
-import scipy
-import glob
 import os
 
 from VQ_model import get_vq_decoder
 from diffusion_model import VQ_diffwave
 import params as params
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class Wav_dataset(Dataset):
     def __init__(self, df, path):
@@ -25,48 +23,48 @@ class Wav_dataset(Dataset):
         self.data_list = []
         for file_name in tqdm(df['filename'].values):
             wav, sr = torchaudio.load(self.path + file_name)
-            resample_wav = torchaudio.toransforms.Resample(sr, 22050)(wav)
+            resample_wav = torchaudio.transforms.Resample(sr, 22050)(wav)
             self.data_list.append(resample_wav)
-        
+
     def __len__(self):
-        return len(self.df):
-    
+        return len(self.df)
+
     def __getitem__(self, idx):
         return self.data_list[idx]
 
 def train(model, encoder_vq, decoder, dataset, noise_scheduler, epochs):
-    data_loader = DataLoader(dataset, batch_size = params.batch_size, shuffle = True)
+    data_loader = DataLoader(dataset, batch_size = params.diff_params['batch_size'], shuffle = True)
     model = model.to(device)
     encoder_vq = encoder_vq.to(device)
     decoder = decoder.to(device)
     criterion = torch.nn.L1Loss()
-    optimizer = toprch.optim.Adam(model.parameters(), lr = params.lr)
-    
+    optimizer = torch.optim.Adam(model.parameters(), lr = params.diff_params['lr'])
+
     for epoch in range(epochs):
         losses = 0.0
         for x in data_loader:
             data = x.to(device)
             vq_z = encoder_vq(data)
-            noise = torch.rando_like(vq_z).to(device)
+            noise = torch.randn_like(vq_z).to(device)
             timesteps = torch.randint(0, noise_scheduler.num_train_timesteps, (noise.shape[0],), device = device)
             noise_vq_z = noise_scheduler(vq_z, noise, timesteps)
             noise_vq_z = noise_vq_z.to(device)
-            
+
             output = model(noise_vq_z, timesteps)
             output = output.squeeze(1)
-            
+
             loss = criterion(output, noise_vq_z)
             optimizer.zero_grad()
             loss.backward()
             losses += loss.item()
             optimizer.step()
-            
+
         print(f'Epoch = {epoch} | loss = {losses:}')
         os.makedirs('model', exist_ok = True)
         torch.save(model.state_dict(), 'model/diffwave.pth')
-        
+
         with torch.no_grad():
-            validation_seed = torch.randn(1, param.embedding_dim).to(device)
+            validation_seed = torch.randn(1, params.vq_params['embedding_dim']).to(device)
             sample = validation_seed.clone()
             for i, t in enumerate(noise_scheduler.timesteps):
                 vq_z = model(sample, t)
@@ -74,17 +72,19 @@ def train(model, encoder_vq, decoder, dataset, noise_scheduler, epochs):
                 sample = sample.squeeze(0)
             sample_wav = decoder(sample)
             os.makedirs('sampling_wav', exist_ok = True)
-            torchaudio.save(f'sampling_wav/epoch={epoch}.wav', sample.cpu(), sample_rate = params.sampling_rate)
+            torchaudio.save(f'sampling_wav/epoch={epoch}.wav', sample_wav.cpu(), sample_rate = params.diff_params['sampling_rate'])
 
 def main():
     path = ''
     df = pd.read_csv(path + 'meta/esc50.csv')
     dataset = Wav_dataset(df, path)
     noise_scheduler = DDPMScheduler(num_train_timesteps = 1000, beta_schedule = 'squaredcos_cap_vs')
-    model = VQ_diffwave(res_channels = params.res_channels, dilation_cycle_length = params.dilation_cycle_length, res_layers = params.res_layers, noise_schedule = noise_scheduler)
+    model = VQ_diffwave(res_channels = params.diff_params['res_channels'],
+                        dilation_cycle_length = params.diff_params['dilation_cycle_length'],
+                        res_layers = params.diff_params['res_layers'],
+                        noise_schedule = noise_scheduler)
     encoder_vq, decoder = get_vq_decoder()
     train(model, encoder_vq, decoder, dataset, noise_scheduler, epochs = params.epochs)
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     main()
-    
---
+++ src/params.py
@@ -0,0 +1,17 @@
+#VQ_model_parameters
+vq_params = {'in_channels': 1,
+            'hidden_dim': 128,
+            'num_residual_layers': 2,
+            'residual_hidden_dim': 32,
+            'embedding_dim': 64,
+            'codebook_size': 512,
+            'batch_size': 16,
+            'lr': 2e-4,}
+
+diff_params = {'res_channels': 128,
+            'dilation_cycle_length': 12,
+            'res_layers': 30,
+            'batch_size': 16,
+            'lr': 2e-4,
+            'epochs': 1000,
+            'sampling_rate': 16000}
